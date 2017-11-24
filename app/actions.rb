a1bbbfa2d3752f3635152cad77d76f5ad1bd632c
Rob Schwitzer <robschwitzer@gmail.com>
2017-11-24 04:44:49 +0000

    Added form, added form validation

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,23 @@ get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
 end
+
+post '/signup' do
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    password   = params[:password]
+    username   = params[:username]
+    
+    @user = User.new({email: email, avatar_url: avatar_url, password: password, username: username})
+    if @user.save
+        "User #{username} saved!" 
+    
+    else
+        erb(:signup)
+    end
+end
+
+get '/signup' do
+    # @user = User.new
+    erb(:signup)
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     has_many :posts
     has_many :comments
     has_many :likes
+    
+    validates_presence_of    :email, :avatar_url, :password, :username
+    validates_uniqueness_of  :email, :password
 end
