9d34cbad71e82366e07b3866bffa3bb3dae6c8c8
Rob Schwitzer <robschwitzer@gmail.com>
2017-12-06 02:05:29 +0000

    added login/signup functionality

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,15 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
 end
+
+
 
 post '/signup' do
     email      = params[:email]
@@ -10,15 +18,69 @@ post '/signup' do
     username   = params[:username]
     
     @user = User.new({email: email, avatar_url: avatar_url, password: password, username: username})
-    if @user.save
-        "User #{username} saved!" 
     
-    else
+    if @user.save
+        redirect to('/login') 
+     else
         erb(:signup)
     end
 end
 
+
+
 get '/signup' do
-    # @user = User.new
+    @user = User.new
     erb(:signup)
 end 
+
+
+
+get '/login' do
+    erb(:login)
+end
+
+
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+      @error_message = "Login Failed"
+      erb(:login)
+    end
+end
+
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+
+get '/posts/new' do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    @post = Post.new({photo_url: photo_url, user_id: current_user.id})
+    if @post.save
+        redirect(to('/'))
+    else
+        erb(:"posts/new")
+    end
+end
+
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb(:"posts/show")
+end
